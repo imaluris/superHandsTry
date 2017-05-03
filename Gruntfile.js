6bda0871296ebc17f52c04ef2b583d32efb5479d
Andrea <andrea.93@hotmail.it>
2017-05-03 14:37:46 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    serve: {
+        path: '<%= pkg.basePath %>'
+    },
+    watch: {
+      files: [ "index.html"],
+      options: {
+        livereload: true,
+      },
+    },
+    concurrent: {
+      startProg: ['watch', 'serve', 'open:dev'],
+			options: {
+				logConcurrentOutput: true
+			}
+    },
+    open : {
+      dev : {
+        path: 'http://localhost:9000/index.html'
+      },
+    }
+  });
+
+  // Load the plugin
+  grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-open');
+  
+  // Default task.
+  grunt.registerTask('start', ['concurrent:startProg']);
+  
+};

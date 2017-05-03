1dfcd527ceddeb8c22c10cbfe97bdf865f11ec37
Andrea <andrea.93@hotmail.it>
2017-05-03 17:29:05 +0200

    naccio

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var serve = require('gulp-serve');
+var open = require('gulp-open');
+ 
+// Basic usage 
+gulp.task('build', function() {
+    // Single entry point to browserify 
+    gulp.src('./src/app.js')
+        .pipe(browserify({
+          insertGlobals : true,
+          debug : !gulp.env.production
+        }))
+        .pipe(gulp.dest('./build/js'))
+});
+
+gulp.task('serve', serve(['public', 'build']));
+
+gulp.task('open', function(){
+  var options = {
+    uri: 'localhost:3000',
+    app: 'chrome'
+  };
+  gulp.src("index.html")
+  .pipe(open(options));
+});
+
+gulp.task('start', ['build', 'serve', 'open']);

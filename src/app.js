26f7bbe7eaa2efe59c0ed3c78dc5be1b0d426ef8
Andrea <andrea.93@hotmail.it>
2017-06-28 12:11:19 +0200

    src folder added

---
+++ src/app.js
@@ -0,0 +1,7 @@
+require('aframe');
+require('aframe-extras');
+require('super-hands');
+require('aframe-teleport-controls');
+require('aframe-physics-components');
+var physics = require('aframe-physics-system');
+physics.registerAll();
